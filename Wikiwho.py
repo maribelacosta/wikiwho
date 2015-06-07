577b0f17e391f00ec708b5429a132386e59a5257
Adam Hyland <protonk@gmail.com>
2015-06-07 14:08:06 -0400

    Make authors an array of author names

--- Wikiwho.py
+++ Wikiwho.py
@@ -540,7 +540,7 @@ def printRevision(revision):
                 #print word
                 #text = text + ' ' + unicode(word.value,'utf-8') + "@@" + str(word.revision) 
                 text.append(word.value)
-                authors.append(word.revision)
+                authors.append(word.author_name)
     print text
     print authors
 
